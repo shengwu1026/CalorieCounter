6cbc2b5aa7003d47d6c9b194402c264442f29d76
wusheng <shengwu1026@gmail.com>
2018-05-08 15:43:09 +1000

    added reset button

--- CalorieCounter/GameViewController.swift
+++ CalorieCounter/GameViewController.swift
@@ -32,7 +32,7 @@ class GameViewController: UIViewController {
 
   @objc func handlePlusButtonTapped() {
     print("Tapped on plus button")
-    addBox()
+    addNode()
   }
     
     // MARK: Minus Button
@@ -50,11 +50,29 @@ class GameViewController: UIViewController {
   } ()
     
     @objc func handleMinusButtonTapped() {
-        print("Tapped on plus button")
+        print("Tapped on minus button")
         removeAllBoxes()
     }
     
+    // MARK: Reset Button
+    let resetButtonWidth = ScreenSize.width * 0.1
+    lazy var resetButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "ResetButton").withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = UIColor(white: 1.0, alpha: 0.7)
+        button.layer.cornerRadius = resetButtonWidth * 0.5
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(handleResetButtonTapped), for: .touchUpInside)
+        button.layer.zPosition = 1
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    } ()
     
+    @objc func handleResetButtonTapped() {
+        print("Tapped on reset button")
+        resetScene()
+    }
+  
   let configuration = ARWorldTrackingConfiguration()
   
   override func viewDidLoad() {
@@ -79,22 +97,53 @@ class GameViewController: UIViewController {
         view.addSubview(minusButton)
         minusButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 12, rightConstant: 24, widthConstant: minusButtonWidth, heightConstant: minusButtonWidth)
         
+        view.addSubview(resetButton)
+        resetButton.anchor(nil, left: nil, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 12, rightConstant: 0, widthConstant: resetButtonWidth, heightConstant: resetButtonWidth)
+        resetButton.anchorCenterYToSuperview()
+        
     }
     
-    func addBox() {
-        let boxNode = SCNNode()
-        boxNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0002)
-        boxNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Material")
-        boxNode.position = SCNVector3(Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5), Float.random(-0.5, max: 0.5))
-        boxNode.name = "box"
-        arView.scene.rootNode.addChildNode(boxNode)
+    func addNode() {
+        let shapeNode = SCNNode()
+        //shapeNode.geometry = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0002)
+        shapeNode.geometry = SCNCapsule(capRadius: 0.05, height: 0.20)
+        shapeNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Material")
+        shapeNode.position = SCNVector3(0.0, 0.0, -0.3)
+        shapeNode.name = "node"
+        arView.scene.rootNode.addChildNode(shapeNode)
     }
     
     func removeAllBoxes() {
         arView.scene.rootNode.enumerateChildNodes{ (node, _) in
-            if node.name == "box" {
+            if node.name == "node" {
                 node.removeFromParentNode()
             }
         }
     }
+    
+    func resetScene() {
+        arView.session.pause()
+        arView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if node.name == "node" {
+                node.removeFromParentNode()
+            }
+        }
+        arView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
